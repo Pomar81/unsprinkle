c87b18651ba50ed4aa26d18f6ae1bf9783a255be
spomaraiko <spomaraiko@exadel.com>
2022-06-19 22:16:13 +0300

    add accessibility to images (exercise 3)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x
           `}
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage alt="poster" src="/images/hero-img.jpg" />
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
   );
 };
